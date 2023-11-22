1b87a947945a4caaacf60b92805b1d55bac37bda
Lollins7 <2982549442@qq.com>
2023-11-22 18:31:13 +0800

    commit

--- python/demo.py
+++ python/demo.py
@@ -1,37 +1,51 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import butter, lfilter
+from collections import defaultdict
 
-# 生成包含高频噪声的示例信号
-fs = 1000  # 采样率
-t = np.arange(0, 1, 1/fs)
-signal = np.sin(2 * np.pi * 50 * t) + 0.5 * np.random.randn(len(t))
 
-# 设计巴特沃斯低通滤波器
-cutoff_frequency = 100  # 截止频率为100 Hz
-order = 4  # 滤波器阶数
+def all_topological_sorts(graph):
+    def dfs(node):
+        nonlocal visited, result
+        visited[node] = True
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+        visited[node] = False
+        result.append(node)
 
-b, a = butter(N=order, Wn=cutoff_frequency/(0.5*fs), btype='low', analog=False)
+    def is_valid_topo_order(order):
+        visited = set()
+        for node in order:
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    return False
+        return True
 
-# 应用滤波器
-filtered_signal = lfilter(b, a, signal)
+    result = []
+    visited = defaultdict(bool)
 
-# 绘制结果
-plt.figure(figsize=(10, 6))
+    for node in graph:
+        if not visited[node]:
+            dfs(node)
 
-plt.subplot(2, 1, 1)
-plt.plot(t, signal, label='原始信号')
-plt.title('原始信号')
-plt.xlabel('时间 (秒)')
-plt.ylabel('幅度')
-plt.legend()
+    all_orders = [result[::-1] for result in result]
 
-plt.subplot(2, 1, 2)
-plt.plot(t, filtered_signal, label='滤波后信号', color='orange')
-plt.title('低通滤波后信号')
-plt.xlabel('时间 (秒)')
-plt.ylabel('幅度')
-plt.legend()
+    valid_orders = [
+        order for order in all_orders if is_valid_topo_order(order)]
 
-plt.tight_layout()
-plt.show()
+    return valid_orders
+
+
+# Given directed edges
+edges = [(1, 2), (2, 3), (1, 4), (4, 5), (5, 3), (4, 6), (6, 5)]
+
+# Build graph
+graph = defaultdict(list)
+for edge in edges:
+    graph[edge[0]].append(edge[1])
+
+# Find all possible topological orders
+possible_orders = all_topological_sorts(graph)
+
+# Print all possible topological orders
+for order in possible_orders:
+    print(" -> ".join(map(str, order)))
