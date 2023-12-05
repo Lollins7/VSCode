e445a298294e300b3187afa0977cf03ab56b2ef2
Lollins7 <jieyu8258@gmail.com>
2023-12-05 15:00:37 +0800

    commit

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string abbreviate(string word) {
+    int length = word.length();
+    if (length > 10) {
+        return word[0] + to_strinwg(length - 2) + word[length - 1];
+    } else {
+        return word;
+    }
+}
+
 int main() {
-    int k;
-    cin >> k;
-    if (k % 2 == 0 && k/2 %2 == 0) cout << "YES";
-    else cout << "NO";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+        cout << abbreviate(word) << endl;
+    }
     return 0;
 }
