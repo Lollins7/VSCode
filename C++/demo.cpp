cda4d2f8d4886120e55925062cc1356666d967a3
Lollins7 <jieyu8258@gmail.com>
2023-12-13 23:06:08 +0800

    Refactor C++ code and add Fibonacci function in Python

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -1,35 +1,81 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
+// 打印数组
+void print_array(int *arr, int n)
 {
-	int t;
-	std::cin >> t;
-
-	for (int i = 0; i < t; i++)
+	if (n == 0)
 	{
-		std::string position;
-		std::cin >> position;
+		printf("ERROR: Array length is ZERO\n");
+		return;
+	}
+	printf("%d", arr[0]);
+	for (int i = 1; i < n; i++)
+		printf(" %d", arr[i]);
+	printf("\n");
+}
 
-		int column = position[0] - 'a';
-		int row = position[1] - '1';
+int *sort_array(int *arr, int n)
+{
+	int i;
+	int maxValue = arr[0];
+	for (i = 1; i < n; i++)
+		if (arr[i] > maxValue) // 输入数据的最大值
+			maxValue = arr[i];
 
-		for (int j = 0; j < 8; j++)
+	// 设置10个桶，依次0，1，，，9
+	const int bucketCnt = 10;
+	vector<int> buckets[bucketCnt];
+	// 桶的大小bucketSize根据数组最大值确定：比如最大值99， 桶大小10
+	// 最大值999，桶大小100
+	// 根据最高位数字映射到相应的桶，映射函数为 arr[i]/bucketSize
+	int bucketSize = 1;
+	while (maxValue)
+	{ // 求最大尺寸
+		maxValue /= 10;
+		bucketSize *= 10;
+	}
+	bucketSize /= 10; // 桶的个数
+	// 入桶
+	for (int i = 0; i < n; i++ i)
+	{
+		int idx = arr[i] / bucketSize; // 放入对应的桶
+		buckets[idx].push_back(arr[i]);
+		// 对该桶使用插入排序(因为数据过少，插入排序即可)，维持该桶的有序性
+		for (int j = int(buckets[idx].size()) - 1; j > 0; j--)
 		{
-			if (j != row)
+			if (buckets[idx][j] < buckets[idx][j - 1])
 			{
-				std::cout << position[0] << j + 1 << std::endl;
-			}
-		}
-
-		for (int k = 0; k < 8; k++)
-		{
-			if (k != column)
-			{
-				std::cout << char(k + 'a') << position[1] << std::endl;
+				swap(buckets[idx][j], buckets[idx][j - 1]);
 			}
 		}
 	}
+	// 顺序访问桶，得到有序数组
+	for (int i = 0, k = 0; i < bucketCnt; i++)
+	{
+		for (int j = 0; j < int(buckets[i].size()); j++)
+		{
+			arr[k++] = buckets[i][j];
+		}
+	}
+	return arr;
+}
 
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	int *arr;
+	arr = (int *)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	arr = sort_array(arr, n);
+
+	print_array(arr, n);
+
+	system("pause");
 	return 0;
 }
