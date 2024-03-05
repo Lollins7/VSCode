9251d80b5006c3ebbba8d57fca66c386d54ae772
Lollins7 <jieyu8258@gmail.com>
2024-03-05 13:35:46 +0800

    commit

--- R/demo.r
+++ R/demo.r
@@ -1,1 +1,11 @@
-q(0.05/2)
+f <- function(x) {
+    n <- length(x)
+    m <- mean(x)
+    s <- sd(x)
+    phi <- (n * sum(((x - m) / s)^3)) / ((n - 1) * (n - 2))
+    phi
+}
+
+x <- rnorm(100)
+f(x)
+describe.by(x)
